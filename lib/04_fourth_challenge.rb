f7ebd677f1b4cade689b28c12c104cf8b27e94b0
Steven wu <stevenwu91@icloud.com>
2020-09-18 15:08:51 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -8,7 +8,7 @@ def fourth_challenge
         {
           name: "Benvolio",
           age: "17",
-          attitude: "hot-headed",
+          attitude: "worried",
         },
         {
           name: "Mercutio",
